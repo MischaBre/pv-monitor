73f0b5cbc3c8cbf24fe154f0afa2797f03465905
MischaGe <gemsa.michael@gmail.com>
2024-03-03 13:48:25 +0100

    setup done

--- worker/run.py
+++ worker/run.py
@@ -1,7 +1,11 @@
 import time
 import datetime
+from worker.startup import startup
+
+
 
 if __name__ == "__main__":
+    startup()
     while True:
         print(f"{datetime.datetime.now()}: run")
         time.sleep(1)
